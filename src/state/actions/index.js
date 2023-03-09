8fdf82a3ed8838990f3342af960039b56ea2fc55
1G2R3M <108121425+1G2R3M@users.noreply.github.com>
2023-03-09 16:13:32 +0530

    Add files via upload

---
+++ src/state/actions/index.js
@@ -0,0 +1,7 @@
+import { openCart, closeCart } from "./isCartOpen";
+import { addToCart, removeFromCart } from "./cart";
+import { setProducts } from "./product";
+import {setError, setSuccess, setLoading} from "./loadingStates"
+
+
+export{openCart, closeCart, addToCart, removeFromCart, setProducts, setError, setSuccess, setLoading}
